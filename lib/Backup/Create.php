a398fc8da27efe5d9411a652d94bbb7a1c4c42b7
Benjamin Ammann <benjamin.ammann@gbssg.ch>
2018-06-17 23:08:43 +0200

    rsync hard links #8

--- lib/Backup/Create.php
+++ lib/Backup/Create.php
@@ -38,11 +38,18 @@ class Create {
 	/** @var password */
 	protected $password;
 
+	/** @var currentDate */
+	protected $currentDate;
+
+	/** @var rsyncOptions */
+	protected $rsyncOptions;
+
 	/**
 	 * @param string $path
 	 */
 	public function __construct($path) {
 		$this->path = $path;
+		$this->currentDate = date('c');
 	}
 
 	/**
@@ -51,6 +58,20 @@ class Create {
 	public function password($password) {
 		$this->password = $password;
 	}
+
+	/**
+	 * @param string $path
+	 *
+	 * @return string $directory
+	 */
+	 private function getLastBackup($path) {
+		 $directories = scandir($path, SCANDIR_SORT_DESCENDING);
+		 $key = array_search('initial', $directories);
+		 unset($directories[$key]);
+		 $directory = reset($directories);
+		 \OC::$server->getLogger()->warning('Last backup: '.$directory);
+		 return $directory;
+	 }
 
 	/**
 	 * @param string $dbtype
@@ -84,46 +105,79 @@ class Create {
 	}
 
 	/**
-	 * @param string $path
+	 * @param string $sourcePath
+	 * @param string $destPath
 	 */
-	private function copydata($path) {
-		$check = shell_exec('which rsync');
-		if(!empty($check)) {
-			$output = shell_exec('rsync -r '.\OCP\Config::getSystemValue('datadirectory').' '.$path);
-		} else {
-			\OC::$server->getLogger()->error('rsync is not installed',['app' => 'backup',]);
-		}
-	}
+	 private function copyFiles($sourcePath, $destPath) {
+		 $check = shell_exec('which rsync');
+		 if(!empty($check)) {
+			 $output = shell_exec('rsync '.$this->rsyncOptions.' '.$sourcePath.' '.$destPath);
+		 } else {
+			 \OC::$server->getLogger()->error('rsync is not installed',['app' => 'backup',]);
+		 }
+	 }
+
+	 /**
+	  * @param string $path
+		* @param bool $realSize
+		*
+		* @return int $size
+		*/
+	 private function getDirectorySize($path, $realSize = TRUE) {
+		 $check = shell_exec('which du');
+		 if($realSize === TRUE) {
+		 	 $options = '-sk --inodes';
+		 } else {
+		 	 $options = '-sk';
+		 }
+		 if(!empty($check)) {
+			 $cmd = 'du '.$options.' "'.$path.'"';
+			 $output = shell_exec($cmd);
+			 $size = explode("\t", $output);
+			 return $size[0];
+		 } else {
+			 \OC::$server->getLogger()->error('du is not installed',['app' => 'backup',]);
+		 }
+	 }
 
 	/**
 	 * @param string $path
 	 */
-	private function copyconfig($path) {
-		$check = shell_exec('which rsync');
-		if(!empty($check)) {
-			$output = shell_exec('rsync -r '.\OC::$configDir.' '.$path.'/config');
-		} else {
-			\OC::$server->getLogger()->error('rsync is not installed',['app' => 'backup',]);
-		}
-	}
-
-	/**
-	 * @param string $path
-	 */
-	private function writemeta($path) {
-		$meta = array('date' => date('c'), 'instanceid' => \OCP\Config::getSystemValue('instanceid'), 'dbtype' => \OCP\Config::getSystemValue('dbtype'));
+	private function writeMeta($path) {
+		$meta = array('date'				=> $this->currentDate,
+									'directory'		=> $path,
+									'size'				=> $this->getDirectorySize($path, FALSE),
+									'realsize'		=> $this->getDirectorySize($path),
+									'instanceid'	=> \OCP\Config::getSystemValue('instanceid'),
+									'dbtype'			=> \OCP\Config::getSystemValue('dbtype'));
 		file_put_contents($path.'/backup.json', json_encode($meta));
 	}
 
-	/**
-	 */
 	public function create() {
 		\OC::$server->getLogger()->warning('Create backup! Path:'.$this->path.' Password:'.$this->password,['app' => 'backup',]);
+
+		$rsyncOptionsGeneral = '-a -l -z --delete --delete-excluded --exclude=lost+found --exclude=*.swp';
+		$rsyncOptionsIncremental = '-a -l -H -z --link-dest='.$this->path.'/initial';
 
 		if(!is_dir($this->path) || !is_writable($this->path)) {
 			\OC::$server->getLogger()->error('Can\'t access directory '.$this->path,['app' => 'backup',]);
 			exit;
 		}
+
+		if(!file_exists($this->path.'/initial')) {
+			mkdir($this->path.'/initial');
+		}
+
+		if(!file_exists($this->path.'/'.$this->currentDate)) {
+			mkdir($this->path.'/'.$this->currentDate);
+		}
+
+		$lastBackup = $this->getLastBackup($this->path);
+
+		$this->rsyncOptions = $rsyncOptionsGeneral;
+
+		// pre-syncing data directory (shorter maintenance time)
+		$this->copyFiles(\OCP\Config::getSystemValue('datadirectory'), $this->path.'/initial/data');
 
 		$maintainance = \OCP\Config::getSystemValue('maintenance');
 		\OCP\Config::setSystemValue('maintenance',true);
@@ -135,19 +189,21 @@ class Create {
 			\OCP\Config::getSystemValue('dbuser'),
 			\OCP\Config::getSystemValue('dbpassword'),
 			\OCP\Config::getSystemValue('dbhost'),
-			$this->path
+			$this->path.'/'.$this->currentDate
 		);
 
 		// copy config folder
-		$this->copyconfig($this->path);
+		$this->copyFiles(\OC::$configDir, $this->path.'/initial/config');
 
-		// syncing data directory
-		$this->copydata($this->path);
-
-		// write meta file
-		$this->writemeta($this->path);
+		// post-syncing data directory
+		$this->copyFiles(\OCP\Config::getSystemValue('datadirectory'), $this->path.'/initial/data');
 
 		\OCP\Config::setSystemValue('maintenance',$maintainance);
 
+		$this->rsyncOptions = $rsyncOptionsIncremental;
+		$this->copyFiles($this->path.'/initial/', $this->path.'/'.$lastBackup);
+
+		// write meta file
+		$this->writeMeta($this->path.'/'.$this->currentDate);
 	}
 }
